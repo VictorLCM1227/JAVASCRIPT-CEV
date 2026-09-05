a1a3e4e3f94446b913562c3219f361918ed06236
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-09-05 11:53:40 -0300

    updates

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,1 +1,45 @@
-alert('Olá!')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fAno.value.length == 0 || fAno.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'imagens/menino.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'imagens/teen-boy.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 
+                    
+                )
+            } else {
+                // Idoso
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+            } else if (idade < 21) {
+                //jovem
+            } else if (idade < 50) {
+                //Adulto
+            } else {
+                // Idoso
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    }
+}
